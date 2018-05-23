3d818df9889472c88a2e4ea073104439ad608534
deepfriedbrain1 <afernan5@mail.sfsu.edu>
2018-05-22 18:51:04 -0700

    added image, testing git usage

--- aquarium/src/src/Aquarium.java
+++ aquarium/src/src/Aquarium.java
@@ -1,6 +1,8 @@
 package src;
 import java.awt.Frame;
+import java.awt.Image;
 import java.awt.MediaTracker;
+import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 /**
@@ -9,6 +11,9 @@ import java.awt.event.WindowEvent;
  */
 public class Aquarium extends Frame 
 {
+    //Images for application
+    Image aquariumBackground;
+    Image [] fishImages = new Image[2];
     //Alerts the user if there is an issue loading images
     MediaTracker tracker;
     
@@ -17,6 +22,9 @@ public class Aquarium extends Frame
         setTitle("The Aquarium");
         
         tracker = new MediaTracker(this);
+        
+        fishImages[0] = Toolkit.getDefaultToolkit().getImage("fish1.gif");
+        tracker.addImage(fishImages[0], 0);
         
         this.addWindowListener(new WindowAdapter(){
             public void windowClosing(
